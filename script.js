12ef684e60767cf2af6fc2e3c46b6185e3ec942c
OmarKh006 <khairyomar006@gmail.com>
2026-04-10 17:46:46 +0200

    adding numbers to screen

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const themeButton = document.querySelector(".themes__toggle");
+const calculatorButtons = document.querySelectorAll(`[data-type]`);
+const calcResult = document.querySelector(".calc__result");
 
 const toggleTheme = () => {
   themeButton.classList.toggle("themes__toggle--isActive");
@@ -10,3 +12,27 @@ const toggleThemeWithEnter = (event) => {
 themeButton.addEventListener("keydown", (e) => toggleThemeWithEnter(e));
 
 themeButton.addEventListener("click", toggleTheme);
+
+let storedNumber = "";
+let currentNumber = "";
+let operation = "";
+
+const updateUI = (value) => {
+  calcResult.innerText = !value ? "0" : value;
+};
+
+const numberButtonHandler = (value) => {
+  if (value === "." && currentNumber.includes(".")) return;
+  if (value === "0" && !currentNumber) return;
+  currentNumber += value;
+  updateUI(currentNumber);
+};
+
+const keysHandler = (button) => {
+  button.addEventListener("click", () => {
+    button.dataset.type === "number" &&
+      numberButtonHandler(button.dataset.value);
+  });
+};
+
+calculatorButtons.forEach(keysHandler);
