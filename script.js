e98d9b3f48a3407e695dba4c8e612e26a8f5bf97
OmarKh006 <khairyomar006@gmail.com>
2026-04-10 17:18:57 +0200

    toggle theme functionality ✅

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test");
+const themeButton = document.querySelector(".themes__toggle");
+
+const toggleTheme = () => {
+  themeButton.classList.toggle("themes__toggle--isActive");
+};
+const toggleThemeWithEnter = (event) => {
+  event.key === "Enter" && toggleTheme();
+};
+
+themeButton.addEventListener("keydown", (e) => toggleThemeWithEnter(e));
+
+themeButton.addEventListener("click", toggleTheme);
